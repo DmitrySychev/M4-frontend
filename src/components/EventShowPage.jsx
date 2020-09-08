3531974b85cd12522b949e789e65c0d4dc2ef87b
Dmitry <axiumone@gmail.com>
2020-09-08 18:24:05 -0400

    building show page

--- src/components/EventShowPage.jsx
+++ src/components/EventShowPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Segment, Container, Header } from 'semantic-ui-react'
+import { Segment, Container, Header, Image } from 'semantic-ui-react'
 import Footer from '../components/Footer.jsx'
-import Navbar from '../components/Navbar.jsx'
+
 
 class EventShowPage extends React.Component{
 
@@ -20,27 +20,38 @@ class EventShowPage extends React.Component{
     return (
 
       this.state.event ? 
-        <>
-        <Container>
-          <br></br>
-          <br></br>
-          <br></br>
-            <Segment >
-        
-            </Segment>
-              
-            <Segment >
-                <Header text='title'>{this.state.event.title}</Header>
+        <div style={{paddingTop: 10 + 'em'}} >
+        <Container className="ui very padded segment" >
+
+          { this.state.event.thumbnail ?
+
+            <Segment size='large'>
+            <Image src={this.state.event.thumbnail} />
             </Segment>
 
-            <Segment >
+            :
+
+           <Segment size='large'>
+              <Image src='https://ca-times.brightspotcdn.com/dims4/default/90f23c8/2147483647/strip/true/crop/2400x1600+0+0/resize/1486x991!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F91%2F37%2Ff1c988db40109234e505b4891a05%2Fla-zoom-party-etiquette.jpg' />
+            </Segment>
+         }
+
+            <Segment size='large'>
+              <Header text='title' textAlign='center'>{this.state.event.title}</Header>
+            </Segment>
+
+            <Segment text textAlign='center'>
+                {this.state.event.description}
+            </Segment>
+
+            <Segment size='large'>
                 <Header>All Users Attending Event</Header>
             </Segment>
 
         </Container>
 
         <Footer />
-        </>
+        </div>
 
         :
 
