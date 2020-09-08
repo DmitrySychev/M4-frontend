6feae3896f40339fdeedae4c5dfe9782da438988
Dmitry <axiumone@gmail.com>
2020-09-08 16:56:08 -0400

    comments

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -14,6 +14,7 @@ class EventCard extends React.Component{
 
   userStatus = () => {
     
+    // refactored logic
     if (this.props.user) {
         if (this.props.joinedEvents !== undefined) {
           if (this.findJoinedEventsId()) {
